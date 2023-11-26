8792c128971f53575188c19bdb9aec8ce166d33d
Pierohc <pieritohc@hotmail.com>
2023-11-26 17:03:27 -0500

    final

--- base de datos/lab_9.sql
+++ base de datos/lab_9.sql
@@ -3,6 +3,9 @@
 -- Host: 127.0.0.1    Database: lab_9
 -- ------------------------------------------------------
 -- Server version	8.0.33
+CREATE DATABASE IF NOT EXISTS `lab_9`;
+USE `lab_9`;
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
